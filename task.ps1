a09d29ae67f15fa0a2b3bbbe5cac207c44ee0064
Alina Bondarenko <alinamoskovchuk18@gmail.com>
2025-08-21 16:17:05 +0300

    solution

--- task.ps1
+++ task.ps1
@@ -1,1 +1,21 @@
-# Write your code here
+$vmSize = "Standard_B2pts_v2"
+$dataFolder = "data"
+
+$jsonFiles = Get-ChildItem -Path $dataFolder -Filter *.json
+
+$regions = @()
+
+foreach($file in $jsonFiles){
+  $filePath = $file.FullName
+
+  $vmParameters = Get-content $filePath | ConvertFrom-Json
+
+  $found = $vmParameters | Where-Object {$_.name -eq $vmSize}
+
+  if ($found) {
+    $region = $file.BaseName
+    $regions += $region
+  }
+}
+
+$regions | ConvertTo-Json | Set-Content -Path "result.json"
